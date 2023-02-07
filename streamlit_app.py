4a92dc3009a56f11ed6f2e8bca561a97ef5891ae
jorsnow <124568911+jorsnow@users.noreply.github.com>
2023-02-07 09:43:22 +0100

    Update streamlit_app.py
    Added dataframe and filter for it

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,24 @@
 import streamlit
+import pandas as pd
 
-streamlit.title("My Parents New Healthy Diner")
 
-streamlit.header("Breakfast menu")
-streamlit.text("🥣 Omega 3 & Blueberry Oatmeal")
-streamlit.text("🥗 Kale, Spinach & Rocket Smoothie")
-streamlit.text("🐔 Hard-Bioled Free-Range Egg")
-streamlit.text("🥑🍞 Avocado Toast")
+streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('Breakfast menu')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Bioled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+/*********************************************/
+my_fruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+my_fruit_list = my_fruit_list.set_index('Fruit')
+
+fruits_selected = streamlit.multiselect('Pick some fruits:', list(my_fruit_list.index), ['Avocado','Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_to_show]
+streamlit.dataframe(fruits_to_show)
+
+/*********************************************/
+
